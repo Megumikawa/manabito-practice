6a529c5da964bebc25414ea8aba4300666f80252
MEGUMI <megumi19901016@gmail.com>
2020-03-24 15:13:14 +0900

    bxsliderの両サイドに写真表示を追加し、jsにも追加

--- feuille/js/script.js
+++ feuille/js/script.js
@@ -4,18 +4,21 @@ $(function(){
   // let menuSpeed = 1000;
   const MENUSPEED = 300;
 
-  $('#top-slider').bxSlider({
-    auto: true
+  $('#top_slider').bxSlider({
+    // maxSlides: 3,
+    // minSlides: 1,
+    // slideMargin: 1000,
+    // moveSlides: 1
   });
 
   $('#pagetop').on('click',function(){
     $('html,body').animate({scrollTop:0},500);
   });
 
-  let menuContents = $('#js-menu-contents');
+  let menuContents = $('#js_menu_contents');
 
   // if(menuContents.length > 0) {
-    $('#js-btn-menu').on('click',function(){
+    $('#js_btn_menu').on('click',function(){
       //$('#js-container').addClass('class名');
       if ($(this).hasClass('open') ===true) {
         $(this).removeClass('open');
@@ -28,8 +31,8 @@ $(function(){
       }
     });
 
-    $('.js-btn-close').on('click',function(){
-      $('#js-btn-menu').removeClass('open');
+    $('.js_btn_close').on('click',function(){
+      $('#js_btn_menu').removeClass('open');
       $('html').css('overflow','');
       menuContents.fadeOut(MENUSPEED);
     });
